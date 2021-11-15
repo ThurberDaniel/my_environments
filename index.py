64a5a8561d02f1c5bfcfd14ebf0a3dc3b8bc66e1
ThurberDaniel <phxburg@gmail.com>
2021-11-14 21:52:44 -0700

    b2

--- index.py
+++ index.py
@@ -1,17 +1,6 @@
 import bcrypt
-a = "Z"
-b = 2
-c = 3
-print(a * b ** c)
-
-
-def cool():
-    d = 4
-    v = 10
-
 
 passwd = b's$cret12'
-
 salt = bcrypt.gensalt()
 hashed = bcrypt.hashpw(passwd, salt)
 
