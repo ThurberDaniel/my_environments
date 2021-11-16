9fd17ef33c3601c503bb5b0df1ef5eb11dbe5e0b
ThurberDaniel <phxburg@gmail.com>
2021-11-15 21:59:35 -0700

    w2

--- index.py
+++ index.py
@@ -1,8 +1,38 @@
-import bcrypt
+from flask import Flask, render_template, flash, redirect, request, session, logging, url_for
 
-passwd = b's$cret12'
-salt = bcrypt.gensalt()
-hashed = bcrypt.hashpw(passwd, salt)
+from flask_sqlalchemy import SQLAlchemy
 
-print(salt)
-print(hashed)
+from forms import LoginForm, RegisterForm
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# Now create flask application object
+
+app = Flask(__name__)
+
+# Database Configuration and Creating object of SQLAlchemy
+
+app.config['SECRET_KEY'] = '!9m@S-dThyIlW[pHQbN^'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/auth'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Create User Model which contains id [Auto Generated], name, username, email and password
+
+
+class User(db.Model):
+
+    __tablename__ = 'usertable'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(15), unique=True)
+
+    username = db.Column(db.String(15), unique=True)
+
+    email = db.Column(db.String(50), unique=True)
+
+    password = db.Column(db.String(256), unique=True)
