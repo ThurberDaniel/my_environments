92ca9909f9810afd9038331683197c0698f5649b
ThurberDaniel <phxburg@gmail.com>
2021-11-22 22:16:35 -0700

    update2

--- index.py
+++ index.py
@@ -27,3 +27,5 @@ class User(db.Model):
     username = db.Column(db.String(15), unique=True)
 
     email = db.Column(db.String(50), unique=True)
+
+    password = db.Column(db.String(256), unique=True)
